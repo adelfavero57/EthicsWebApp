bbcdbacc985d10170f50374eecc02ec316c05dc7
Shyam Raj <sraj2279@uni.sydney.edu.au>
2021-09-10 01:25:17 +0000

    forms.py edited online with Bitbucket

--- Project/accounts/forms.py
+++ Project/accounts/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import forms
 from django import forms as forms2
-from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 class CreateUserForm(forms.UserCreationForm):
