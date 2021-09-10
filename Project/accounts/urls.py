3d43974f996e609b621473d58610259c3839a0da
Alex <adel8312@uni.sydney.edu.au>
2021-09-10 16:23:01 +1000

    password_reset1

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 # URLconf
 urlpatterns = [
@@ -7,4 +8,8 @@ urlpatterns = [
     path('register/', views.registerPage),
     path('home/', views.homePage),
     path('edit_profile/', views.editUserPage),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
