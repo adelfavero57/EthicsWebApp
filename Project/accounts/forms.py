47c119a0c67e6d0b6392bf9ef1d2b3032e4af280
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2021-09-10 20:00:46 +1000

    updated invalid username/password

--- Project/accounts/forms.py
+++ Project/accounts/forms.py
@@ -5,6 +5,12 @@ from django import forms as forms2
 
 
 class CreateUserForm(forms.UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        self.fields['email'].required = True
+
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username',
