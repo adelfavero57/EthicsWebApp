61a840582d6bad7f2589dde335a65b01d359bee2
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-09-11 14:38:49 +1000

    add comment

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('home/', views.homePage, name='home'),
     path('edit_profile/', views.editUserPage),
 
-    ##Password reset##
+     ##Password reset## 
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
     path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
