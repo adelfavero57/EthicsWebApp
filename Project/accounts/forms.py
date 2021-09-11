b8a3c6dbf1cfca98d427706e3321fac8ecf03bb4
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-09-11 14:16:45 +1000

    edit

--- Project/accounts/forms.py
+++ Project/accounts/forms.py
@@ -1,7 +1,8 @@
-from django.forms import ModelForm
-from django import forms as forms2
-from django.contrib.auth import forms
+from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import forms
+from django import forms as forms2
+
 
 
 class UpdateUserForm(forms.UserChangeForm):
@@ -23,3 +24,5 @@ class LoginForm(forms2.Form):
     password = forms2.CharField(widget=forms2.PasswordInput)
     remember_me = forms2.BooleanField(required=False)
     custom_error = False
+
+
