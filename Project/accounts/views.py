e5c38d097e2f14c064b4392b2f4ae0b5154715f5
Alex <adel8312@uni.sydney.edu.au>
2021-09-11 12:42:49 +1000

    ss

--- Project/accounts/views.py
+++ Project/accounts/views.py
@@ -2,13 +2,7 @@ from django.http import request
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
-<<<<<<< HEAD
-from django.urls import reverse_lazy
-from django.views import generic
-from .forms import UpdateUserForm
-=======
 from django.contrib import messages
->>>>>>> master
 
 # Create your views here.
 from .forms import CreateUserForm, LoginForm
