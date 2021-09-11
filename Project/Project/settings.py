c2bc963353ae366d6e2234e67fa14b1e4c64a587
Alex <adel8312@uni.sydney.edu.au>
2021-09-11 13:23:16 +1000

    re-doing edit profile

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -126,3 +126,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP Config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS =  True
+EMAIL_HOST_USER = 'pwreset551@gmail.com'
+EMAIL_HOST_PASSWORD = 'g!antPie38'
