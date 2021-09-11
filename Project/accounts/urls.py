7fc6ae4fb9b8a4f7249b18a8e72ffdc7ed0201a7
Alex <adel8312@uni.sydney.edu.au>
2021-09-11 10:25:33 +1000

    password_reset2

--- Project/accounts/urls.py
+++ Project/accounts/urls.py
@@ -8,8 +8,10 @@ urlpatterns = [
     path('register/', views.registerPage),
     path('home/', views.homePage),
     path('edit_profile/', views.editUserPage),
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
+    ##Password reset##
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_sent.html"), name="password_reset_done"),
+    path('reset_password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name="password_reset_complete"),
 ]
