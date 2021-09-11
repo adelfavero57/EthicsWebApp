28c45df574447dda0b7b5db5c3f5475d9b9791a3
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-09-11 14:35:53 +1000

    edit

--- Project/accounts/models.py
+++ Project/accounts/models.py
@@ -1,14 +1,5 @@
 from django.db import models
-from django.contrib.auth.forms import UserCreationForm
+
 # Create your models here.
-
-
-
-
-
-    # write some methods that retrive data from database / get data from database
-
-    
-
 
 
