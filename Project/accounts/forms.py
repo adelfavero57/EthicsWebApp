d73f85478b3bb836b95753bb38fb027982bd8223
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-09-11 13:06:18 +1000

    edit on managelist branch

--- Project/accounts/forms.py
+++ Project/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import NormalUser
 from django import forms as forms2
+from django.contrib.auth.forms import UserCreationForm
 
 
 class CreateUserForm(forms.UserCreationForm):
