8ce2444be31062de8854f6c47d0783f9960bde9f
Alex <adel8312@uni.sydney.edu.au>
2021-09-19 14:02:57 +1000

    resolving conflicts

--- Project/accounts/admin.py
+++ Project/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Application, Question, Answers
+from accounts.models import Application, Question, Answers
 
 # Register your models here.
 admin.site.register(Application)
