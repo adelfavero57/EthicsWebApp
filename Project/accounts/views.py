d6114bc8943a3f9dba81c621e01149a85a468bab
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2021-09-23 22:38:11 +1000

    fixed

--- Project/accounts/views.py
+++ Project/accounts/views.py
@@ -58,7 +58,7 @@ def loginPage(request):
                 elif request.user.groups.filter(name='admin').exists():
                     return redirect('adminpage')
                 else:
-                    logout(user)
+                    logout(request)
                     redirect('login')
 
         # if user is invalid
