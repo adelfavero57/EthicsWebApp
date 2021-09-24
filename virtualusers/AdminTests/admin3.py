40c0821c3b0e03d7263b2e3009c5834673098fec
Zhixin Gao <zincgao@Zhixins-MacBook-Pro.local>
2021-09-24 17:58:07 +1000

    adminTest

---
+++ virtualusers/AdminTests/admin3.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+import os
+
+# Test to see if login is working and identify different user. 
+
+# Get chrome driver
+dir_path = os.path.dirname(os.path.realpath(__file__))
+driver = webdriver.Chrome(dir_path + "/chromedriver")
+driver.set_page_load_timeout(10)
+
+# Run the website on local machine in localhost, port 8000.
+driver.get("http://localhost:8000/login/")
+time.sleep(5)
+driver.find_element_by_name("username").send_keys("zinc")
+time.sleep(5)
+driver.find_element_by_name("password").send_keys("zinc")
+time.sleep(5)
+driver.find_element_by_name("Login").click()
+time.sleep(5)
+
+# Test to see if the normal user can go to adminpage
+driver.get("http://localhost:8000/adminpage/")
+time.sleep(5)
+driver.close()
