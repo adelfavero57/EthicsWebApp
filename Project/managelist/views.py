58bb5d97181e23b85bff421271b70354f34f265a
Alex <adel8312@uni.sydney.edu.au>
2021-09-25 19:05:43 +1000

    implement download form functionality

--- Project/managelist/views.py
+++ Project/managelist/views.py
@@ -2,6 +2,13 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from accounts.decorators import allowed_users
 from django.shortcuts import render, redirect
+
+# PDF generation imports
+from django.http import FileResponse
+import io
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
 # Create your views here.
 
 
@@ -21,6 +28,32 @@ test5 = Application("form5", "20%", "IN PROCESS")
 
 Applications = [test1, test2]
 
+# reportlab documentation for customisation: https://reportlab.com/docs/reportlab-userguide.pdf
+def download_application(request):
+    #create buffer
+    buf = io.BytesIO()
+    #create canvas
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)
+    #create text object
+    textob = c.beginText()
+    textob.setTextOrigin(inch, inch)
+    textob.setFont("Helvetica", 14)
+    
+    #content on pdf
+    content = [
+        "Test line"
+        "test line 2"
+        "test line 3"
+    ]
+    for line in content:
+        textob.textLine(line)
+    
+    c.drawText(textob)
+    c.showPage()
+    c.save()
+    buf.seek(0)
+
+    return FileResponse(buf, as_attachment=True, filename='application.pdf')
 
 def logout_view(request):
     logout(request)
