65333deda600862c0f2ab1c07331ac9389e5bcdf
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2021-09-27 23:01:15 +1000

    styling changes

--- Project/qualifier/views.py
+++ Project/qualifier/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponse
 from accounts.decorators import allowed_users
 from django.shortcuts import render, redirect
+from accounts.models import Question
 # Create your views here.
 
 
@@ -13,5 +15,14 @@ def welcome(request):
 
 
 def quiz(request):
-    context = {}
+    questions = Question.objects.all().filter(section_name="B")
+    if request.method == 'POST':
+        # if one answer is yes, return fail otherwise redirect to ethics welcome.
+        for i in range(201, len(questions)+201):
+            selection = request.POST.get('selection{}'.format(i))
+            if selection == 'Yes':
+                return HttpResponse('Failure')
+        return HttpResponse('Success')
+
+    context = {'questions': questions}
     return render(request, 'quiz.html', context)
