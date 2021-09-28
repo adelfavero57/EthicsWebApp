81499639f32c83532c0658e3c55bad02a5b5d217
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-09-28 13:10:59 +1000

    update

--- Project/managelist/views.py
+++ Project/managelist/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from accounts.decorators import allowed_users
 from django.shortcuts import render, redirect
 from accounts.models import Application
-from django.http import JsonResponse
 
 # PDF generation imports
 from django.http import FileResponse
@@ -13,13 +12,6 @@ from reportlab.lib.units import inch
 from reportlab.lib.pagesizes import letter
 # Create your views here.
 
-test1 = Application("form1", "20%", "IN PROCESS")
-test2 = Application("form2", "20%", "IN PROCESS")
-test3 = Application("form3", "20%", "IN PROCESS")
-test4 = Application("form4", "20%", "IN PROCESS")
-test5 = Application("form5", "20%", "IN PROCESS")
-
-Applications = [test1, test2]
 
 # reportlab documentation for customisation: https://reportlab.com/docs/reportlab-userguide.pdf
 def download_application(request):
@@ -63,26 +55,6 @@ def managelistPage(request):
 
     return render(request, 'managelist.html', context)
 
-# @login_required(login_url='login')
-# @allowed_users(allowed_roles=['student'])
-# def search(request):
-    
-#     data = dict()
-
-#     if request.method == "POST":
-
-#         search_text = request.POST["search_text"]
-
-#         if search_text is not None:
-
-#             applications = Application.objects.filter(user=request.user, title=search_text)         
-
-#             data['result'] = applications
-
-            
-#     print(data)
-
-#     return JsonResponse(data)
     
 
 @login_required(login_url='login')
