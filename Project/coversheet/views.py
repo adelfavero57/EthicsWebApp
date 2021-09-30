40f1f3e1914757fa85846d8e6b51e4b9d86f87cf
Shuai Sun <ssun5590@uni.sydney.edu.au>
2021-09-30 11:18:50 +0800

    1

--- Project/coversheet/views.py
+++ Project/coversheet/views.py
@@ -1,18 +1,12 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.db.models.fields import IntegerField
 from django.shortcuts import render, redirect
 from django.shortcuts import render, redirect
 from accounts.models import CoverSheetQuestion
+from accounts.models import CoverSheetAnswers
+from accounts.models import Application
 # Create your views here.
-
-
-class Application:
-    def __init__(self, name, process, status):
-
-        self.name = name
-        self.process = process
-        self.status = status
-
 
 
 def logout_view(request):
@@ -22,6 +16,21 @@ def logout_view(request):
 
 @login_required(login_url='login')
 def coversheetPage(request):
+    if request.method == 'POST':
+        summary_text = request.POST['summary']
+        csq_id = CoverSheetQuestion.objects.get(pk=1001)
+        a_id = Application.objects.get(pk=5)
+        Summary = CoverSheetAnswers.objects.create(text = summary_text, question_id = csq_id, application_id = a_id, is_short_answer = True)
+        Summary.save()
+
+      
+        Protocol_Title_text = request.POST['protocol']
+        csq_id = CoverSheetQuestion.objects.get(pk=1002)
+        a_id = Application.objects.get(pk=5)
+        Protocol_Title = CoverSheetAnswers.objects.create(text = Protocol_Title_text, question_id = csq_id, application_id = a_id, is_short_answer = True)
+        Protocol_Title.save()
+    
+
     cover = CoverSheetQuestion.objects.all()
     context = {'cover': cover}
     return render(request, 'coversheet.html', context)
