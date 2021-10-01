90862eaf9a2bcf68ad5e7b139b11b5af96d10460
Shuai Sun <ssun5590@uni.sydney.edu.au>
2021-10-01 16:17:13 +0800

    create a new application id when filling in the coversheet

--- Project/coversheet/views.py
+++ Project/coversheet/views.py
@@ -18,14 +18,9 @@ def logout_view(request):
 def coversheetPage(request):
     if request.method == 'POST':
 
-        # added by Zinc and Ben
-        app = Application.objects.create(user = request.user, title = 'test', supervisor = 'zinc', status = 'in progress')
-        app.save()
-        latest = Application.objects.latest('id')
-        a_id = Application.objects.get(latest.pk)
-        #print(latest.pk)
-    
-        #a_id = Application.objects.get(pk=1)
+        Application.objects.create(user = request.user, title = 'fix the of twice application', supervisor = 'shuai', status = 'in progress')
+        a_id = Application.objects.latest('id')
+        
         summary_text = request.POST['summary']
         csq_id_1001 = CoverSheetQuestion.objects.get(pk=1001)
         Summary = CoverSheetAnswers.objects.create(text = summary_text, question_id = csq_id_1001, application_id = a_id, is_short_answer = True)
