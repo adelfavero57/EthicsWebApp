b0090983efd5e99b69ea56fc47c7282156de714c
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-10-01 12:02:25 +1000

    change

--- Project/coversheet/urls.py
+++ Project/coversheet/urls.py
@@ -9,9 +9,5 @@ from accounts.views import editUserPage
 urlpatterns = [
     path('', views.coversheetPage, name='coversheet'),
     path('logout/', views.logout_view, name='logout'),
-<<<<<<< HEAD
-    path('edit_profile/', editUserPage, name='editprofile'),
-=======
     path('edit_profile/', editUserPage, name='editprofile')
->>>>>>> 3f455360ff42d02ad6c646f72bf96ab9cbba2756
 ]
