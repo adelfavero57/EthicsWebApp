e714fdc99fc4a48da6d33cc461c950226c49b99f
Alex <adel8312@uni.sydney.edu.au>
2021-10-02 10:09:11 +1000

    project_name attribute

--- Project/accounts/models.py
+++ Project/accounts/models.py
@@ -15,6 +15,7 @@ class Application(models.Model):
     title = models.TextField(max_length=200, null=True)
     supervisor = models.TextField(max_length=150, null=True)
     status = models.TextField(max_length=10, null=True)
+    project_name= models.TextField(max_length=100, null=True)
     
 
 class Question(models.Model):
