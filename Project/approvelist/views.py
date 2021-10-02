5b91895ffc8d5f99b2ed9533678e8e3aa15edece
sraj2279 <68522239+sraj2279@users.noreply.github.com>
2021-10-02 11:37:18 +1000

    view back-end completed

--- Project/approvelist/views.py
+++ Project/approvelist/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from accounts.decorators import allowed_users
-from accounts.models import Application
+from accounts.models import Answers, Application
+from accounts.models import Question
 # Create your views here.
 
 
@@ -29,8 +30,19 @@ def disapprove(request, item_id):
     item = Application.objects.get(pk=item_id)
     item.status = 'disapproved'
     item.save()
+    context = {'applications': applications}
 
     return redirect('approvelist')
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['staff'])
+def viewPage(request, item_id):
+    ques = Question.objects.all()
+    ans = Answers.objects.all().filter(id=item_id)
+
+    context = {'ques': ques, 'ans': ans}
+    return render(request, 'view.html', context)
 
 
 @login_required(login_url='login')
