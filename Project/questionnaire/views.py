200ab7c34878fdde634b990240ba091245daa23a
Zhixin Gao <zincgao@Zhixins-MacBook-Pro.local>
2021-10-03 15:29:16 +1100

    admin

--- Project/questionnaire/views.py
+++ Project/questionnaire/views.py
@@ -12,24 +12,24 @@ from django.http import HttpResponse
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['researcher'])
 def questionnaire(request, application_id):
-    print(application_id)
+    
     a_id = Application.objects.get(pk=application_id)
     questions = Question.objects.all()
     #print(request.method)
     if request.method == 'POST':
 
-        print(request.POST['name'])
         for i in questions:
             print("the number is", i.question_num)
             if i.question_num == 905:
-                return redirect('managelist')
+                break
             temp2 = str(i.question_num)
             temp = request.POST[temp2]
             
             temp_obj = Question.objects.get(pk = i.question_num)
-            new_answer_obj = Answers.objects.create(text = temp, question_id = i, 
+            new_answer_obj = Answers.objects.create(short_answer_text = temp, question_id = i, 
             application_id = a_id, is_short_answer = 0, section_name = i.section_name)
-        
+        a_id.status = "COMPLETE"
+        a_id.save()
         return redirect('managelist')
 
     
