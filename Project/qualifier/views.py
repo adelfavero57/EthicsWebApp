2edabb5398170a8a791527ae494f3280df42cda5
Zhixin Gao <zincgao@Zhixins-MacBook-Pro.local>
2021-10-07 20:25:08 +1100

    create answers object in success page

--- Project/qualifier/views.py
+++ Project/qualifier/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from accounts.decorators import allowed_users
 from django.shortcuts import render, redirect
 from accounts.models import Question
-from accounts.models import Application
+from accounts.models import Application, Answers
 from django.http import HttpResponse
 # Create your views here.
 
@@ -30,7 +30,19 @@ def failure(request):
 @allowed_users(allowed_roles=['researcher'])
 def success(request):
     if request.method == 'POST':
+        if len(Application.objects.all()) == 0:
+            new_id = 100
+        else:
+            new_id = Application.objects.latest("id").id + 1
+            new_application = Application.objects.create(id = new_id, user=request.user, status = "IN PROGRESS", supervisor='', title='')
         
+
+
+        questions = Question.objects.all()
+        for i in questions:
+            new_answer_obj = Answers.objects.create(question_id = i, 
+            application_id = new_application, is_short_answer = 0, section_name = i.section_name,
+            is_referenced = 0, is_exemplar = 0)
         # request.session['finish'] = True
         return redirect('coversheet')
     return render(request, 'success.html')
