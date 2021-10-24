5230ce64ceeeb256a750ee2a05eee1290a56e654
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-10-24 23:03:13 +1100

    unit testing and coverage

--- Project/accounts/admin.py
+++ Project/accounts/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from accounts.models import Application, Question, Answers, CoverSheetAnswers, CoverSheetQuestion
 
+class MyAdminSite(admin.AdminSite):
+
+    site_header = "Admin area"
+
+admin_site = MyAdminSite(name='myadmin')
 # Register your models here.
-admin.site.register(Application)
-admin.site.register(Question)
-admin.site.register(Answers)
-admin.site.register(CoverSheetQuestion)
-admin.site.register(CoverSheetAnswers)
+admin_site.register(Application)
+admin_site.register(Question)
+admin_site.register(Answers)
+admin_site.register(CoverSheetQuestion)
+admin_site.register(CoverSheetAnswers)
--- Project/accounts/apps.py
+++ Project/accounts/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+from django.contrib.admin.apps import AdminConfig
+
+class MyAdminConfig(AdminConfig):
+    default_site = 'accounts.admin.MyAdminSite'
+
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
