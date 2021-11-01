5bf58182afc1e134e5b46315ccd5aa2a2ec5676b
Alvin Peng <cpen2847@uni.sydney.edu.au>
2021-11-02 09:46:59 +1100

    change the admin page

--- Project/accounts/admin.py
+++ Project/accounts/admin.py
@@ -5,10 +5,11 @@ class MyAdminSite(admin.AdminSite):
 
     site_header = "Admin area"
 
+
 admin_site = MyAdminSite(name='myadmin')
 # Register your models here.
 admin_site.register(Application)
 admin_site.register(Question)
-admin_site.register(Answers)
-admin_site.register(CoverSheetQuestion)
-admin_site.register(CoverSheetAnswers)
+admin.site.register(Answers)
+admin.site.register(CoverSheetQuestion)
+admin.site.register(CoverSheetAnswers)
