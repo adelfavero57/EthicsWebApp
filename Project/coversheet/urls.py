302dcbb222f8c16c2a079daae41619b5cefd2569
Shuai Sun <ssun5590@uni.sydney.edu.au>
2021-11-03 07:56:33 +0800

    dropdown menu

--- Project/coversheet/urls.py
+++ Project/coversheet/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('edit_profile/', editUserPage, name='editprofile'),
     path('<application_id>', questionnaire, name="questionnaire"),
     
-    path('PIS/<int:pk>', PISform2.as_view(), name='information_sheet'),
-    path('PCF/<int:pk>', PCFform2.as_view(), name='consent_form'),
-    path('new_PIS/<int:pk>', createPIS.as_view(), name='new_PIS'),
-    path('new_PCF/<int:pk>', createPCF.as_view(), name='new_PCF'),
+    path('PIS/<int:pk>', PISform2, name='information_sheet'),
+    path('PCF/<int:pk>', PCFform2, name='consent_form'),
+    path('new_PIS/<int:pk>', createPIS, name='new_PIS'),
+    path('new_PCF/<int:pk>', createPCF, name='new_PCF'),
 ]
